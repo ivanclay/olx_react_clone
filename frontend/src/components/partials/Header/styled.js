618e98e7117866b47f19e32653584b5d27886a93
ivanclay <ivan.clay.moura@gmail.com>
2021-09-26 10:35:47 -0300

    Styling the 'Header' component.

--- frontend/src/components/partials/Header/styled.js
+++ frontend/src/components/partials/Header/styled.js
@@ -8,31 +8,73 @@ export const HeaderArea = styled.div`
         max-width: 1000px;
         margin: auto;
         display: flex;
+
+        a { text-decoration: none;}
+
+        .logo {
+            display: flex;
+            flex: 1;
+            align-items: center;
+
+            .logo-1, .logo-2, .logo-3 {
+                font-size: 27px;
+                font-weight: bold;
+            }
+
+            .logo-1 {
+                color: #FF0000;
+            }
+
+            .logo-2 {
+                color: #00FF00;
+            }
+
+            .logo-3 {
+                color: #0000FF;
+            }
+        }
+        nav {
+            padding-top: 10px;
+            padding-bottom: 10px;
+
+            ul, li {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+            }
+
+            ul {
+                display: flex;
+                align-items: center;
+                height: 40px;
+            }
+
+            li{
+                margin-left: 20px;
+                margin-right: 20px;
+
+                a {
+                    color: #000;
+                    font-size: 14px;
+                    text-decoration: none;
+
+                    &:hover {
+                        color: #999;
+                    }
+
+                    &.button {
+                        background-color: #FF8100;
+                        border-radius: 4px;
+                        padding: 5px 10px;
+                        color: #FFF;
+                        font-weight: bold;
+                    }
+
+                    &.button:hover {
+                        background-color: #E57706;
+                    }
+                }
+            }
+        }
     }
-
-    a { text-decoration: none;}
-
-    .logo {
-        display: flex;
-        flex: 1;
-        align-items: center;
-
-        .logo-1, .logo-2, .logo-3 {
-            font-size: 27px;
-            font-weight: bold;
-        }
-
-        .logo-1 {
-            color: #FF0000;
-        }
-
-        .logo-2 {
-            color: #00FF00;
-        }
-
-        .logo-3 {
-            color: #0000FF;
-        }
-    }
-    
 `;
